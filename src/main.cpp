97aaa3669c93862fe9beaa0e2175adf0f331cd12
KaroKru <92943379+KaroKru@users.noreply.github.com>
2026-06-30 17:48:07 +0200

    added Task

--- src/main.cpp
+++ src/main.cpp
@@ -8,23 +8,69 @@
 #include <memory>
 #include "Dispatcher.hpp"
 #include "TaskFactory.hpp"
+#include <limits>
 
 namespace
 {
-void open()
+TaskType selectTask()
+{
+    while (true)
+    {
+        std::cout << "Select task type:\n"
+                  << "1. Show logs\n"
+                  << "2. Save logs\n"
+                  << "Select: ";
+
+        int choice{};
+
+        if (!(std::cin >> choice))
+        {
+            std::cin.clear();
+            std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+
+            std::cout << "Invalid input, please enter a number" << std::endl;
+            continue;
+        }
+
+        switch (choice)
+        {
+            case 1:
+                return TaskType::Show;
+
+            case 2:
+                return TaskType::Save;
+
+            default:
+                std::cout << "Invalid option. Please try again.\n\n";
+        }
+    }
+}
+
+void open(TaskType taskType)
 {
     const std::string path = "../src/log.txt";
     const FileReader openFile(path);
-    auto task = TaskFactory::create(TaskType::Show);
+
+    auto task = TaskFactory::create(taskType);
+
+    if (!task)
+    {
+        std::cerr << "Failed.\n";
+        return;
+    }
+
     Dispatcher dispatcher(std::move(task));
-    
+
     const std::vector<std::string> line = openFile.readFile();
 
     for (const auto& iValue : line)
     {
         const std::unique_ptr<ILogEntry> entryData = LogParser::parseLine(iValue);
 
-        dispatcher.registerInformation(*entryData);
+        if (entryData)
+        {
+            dispatcher.registerInformation(*entryData);
+        }
     }
 
     dispatcher.dispatchInformation();
@@ -34,7 +80,7 @@ void open()
 int main()
 {
     std::cout << "check message" << std::endl;
-    open();
+    open(selectTask());
 
     return 0;
 }
